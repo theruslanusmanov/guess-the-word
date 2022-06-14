b0969786ea6d9def4cba620ace5168ab455e8876
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-06-14 09:51:49 +0500

    make a guess

--- GuessTheWord/Models/Guess.swift
+++ GuessTheWord/Models/Guess.swift
@@ -32,6 +32,20 @@
 
 import Foundation
 
+enum GuessStatus {
+  case pending
+  case complete
+  case invalidWord
+}
+
 struct Guess: Identifiable {
   var id = UUID()
+  var word: [GuessedLetter] = []
+  var status: GuessStatus = .pending
+  
+  var letters: String {
+    return word.reduce("") { partialResult, nextLetter in
+      partialResult.appending(nextLetter.letter)
+    }
+  }
 }
