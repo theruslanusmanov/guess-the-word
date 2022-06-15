31f3dcca873d59dba1d8096dea8627e414be29a1
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-06-15 10:10:33 +0500

    show the guess

--- GuessTheWord/GameBoardViews/CurrentGuessView.swift
+++ GuessTheWord/GameBoardViews/CurrentGuessView.swift
@@ -34,13 +34,57 @@
 import SwiftUI
 
 struct CurrentGuessView: View {
+  @Binding var guess: Guess
+  var wordLength: Int
+  
+  var unguessedLetters: Int {
+    wordLength - guess.word.count
+  }
+  
   var body: some View {
-    Text("Placeholder")
+    GeometryReader { proxy in
+      HStack {
+        Spacer()
+        let width = (proxy.size.width - 40) / 5 * 0.8
+        ForEach(guess.word.indices, id: \.self) { index in
+          let letter = guess.word[index]
+          GuessBoxView(letter: letter, size: width, index: index)
+        }
+        ForEach(0..<unguessedLetters, id: \.self) { _ in
+          EmptyBoxView(size: width)
+        }
+        Spacer()
+      }
+      .padding(5.0)
+      .overlay(
+        Group {
+          if guess.status == .invalidWord {
+            Text("Word not in  dictionary.")
+              .foregroundColor(.red)
+              .background(Color(UIColor.systemBackground).opacity(0.8))
+              .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                  guess.status = .pending
+                }
+              }
+          }
+        }
+      )
+    }
   }
 }
 
 struct CurrentGuessView_Previews: PreviewProvider {
   static var previews: some View {
-    CurrentGuessView()
+    let guessedLetter = GuessedLetter(letter: "S", status: .inPosition)
+    let guessedLetter2 = GuessedLetter(letter: "A", status: .notInPosition)
+    let guess = Guess(
+      word: [guessedLetter, guessedLetter2],
+      status: .pending
+    )
+    CurrentGuessView(
+      guess: .constant(guess),
+      wordLength: 5
+    )
   }
 }
